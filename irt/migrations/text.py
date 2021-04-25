9aa24de6eb36ece328c9b638b9e3409d7460d2da
Felix Hamann <felix@hamann.xyz>
2021-04-25 14:29:09 +0200

    added first migration

---
+++ irt/migrations/text.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+migrate legacy ryn text datasets to the new irt format
+
+"""
+
+import irt
+from irt.common import helper
+from irt.common import logging
+from irt.text import selector
+from irt.text import loader as text_loader
+
+import re
+import gzip
+import pathlib
+from collections import deque
+from collections import defaultdict
+
+from typing import Optional
+
+
+log = logging.get("migrate.text")
+
+
+SEP = "|"
+
+
+class Legacy(text_loader.Loader):
+
+    path: pathlib.Path
+
+    class Selector(text_loader.Selector):
+
+        e2result: dict[int, tuple[str, str]]
+
+        def __init__(self, e2result):
+            self.e2result = e2result
+
+        def by_entity(self, e: int) -> Optional[text_loader.Result]:
+            if e not in self.e2result:
+                return None
+
+            mentions, blobs = zip(*self.e2result[e])
+            res = text_loader.Result(mentions=mentions, blobs=blobs)
+            return res
+
+    def _read(self, path, fname):
+        with gzip.open(str(path / fname), mode="r") as fd:
+            log.info(f"reading {path.name}/{fname}")
+            fd.readline()  # consume head comment
+
+            for line in map(bytes.decode, fd.readlines()):
+                e_str, label, sentence = map(str.strip, line.split(SEP, maxsplit=2))
+                yield int(e_str), label, sentence
+
+    def _read_mult(self, path, *fnames):
+        for fname in fnames:
+            yield from self._read(path, fname)
+
+    def __init__(self, path):
+        self.path = helper.path(path, exists=True)
+
+    def __enter__(self):
+
+        # optionally reverse engineer the mention from
+        # the marked dataset
+
+        files = (
+            "cw.train-sentences.txt.gz",
+            "ow.valid-sentences.txt.gz",
+            "ow.test-sentences.txt.gz",
+        )
+
+        e2mention = defaultdict(deque)
+        marked_path = self.path / "bert-base-cased.30.768.marked"
+
+        if marked_path.exists():
+            count = 0
+            rex = re.compile(r"\[MENTION_START\](.+?)\[MENTION_END\]")
+            for e, label, sentence in self._read_mult(marked_path, *files):
+                match = re.search(rex, sentence)
+
+                if match:
+                    mention = match[1].strip()
+                else:
+                    mention = ""
+                    count += 1
+
+                e2mention[e].append(mention)
+
+            log.info(f"there are {count} sentences without explicit mentions")
+
+        e2result = defaultdict(list)
+        clean_path = self.path / "bert-base-cased.30.768.clean"
+        for e, label, sentence in self._read_mult(clean_path, *files):
+            mention = e2mention[e].popleft() if e in e2mention else label
+            e2result[e].append((mention, sentence))
+
+        return Legacy.Selector(e2result=e2result)
+
+    def __exit__(self, *_):
+        pass
+
+
+if __name__ == "__main__":
+
+    # irt-cde conversion 21/04/25
+    selector.create(
+        loader=Legacy(
+            irt.ENV.SRC_DIR / "legacy/cde.m_8051991_27/contexts-v7-2020-12-31.db"
+        ),
+        path=irt.ENV.DATASET_DIR / "irt-cde",
+        sentences=30,
+        seed=8051991,
+        shuffle=False,
+        mark=True,
+        mask=True,
+    )
+
+    # irt-fb conversion 21/04/25
+    pass
