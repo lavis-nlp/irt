59f2ab09c26f6dd8c10bdfbc46b4297947201aea
Felix Hamann <felix@hamann.xyz>
2021-07-23 15:50:44 +0200

    updated installation procedure

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup
 
 setup(
-    name='irt',
-    version='1.0',
-    packages=['irt'],
-    license='MIT',
-    long_description=open('README.md').read(),
+    name="irt-data",
+    version="1.0",
+    packages=["irt"],
+    license="MIT",
+    author="Felix Hamann",
+    author_email="felix@hamann.xyz",
+    description="Inductive Reasoning with Text - Benchmarks",
+    install_requires=[
+        "pyyaml==5.*",
+        "networkx==2.*",
+        "gitpython==3.*",
+        "tqdm",
+        "jupyter",
+        "tabulate",
+        "matplotlib",
+    ],
 )
